743c7d2125ffb5f2229b5e46cee8af89027220ec
Chris Morton <chris.morton@certifi.com>
2025-01-18 13:45:51 -0600

    stash

---
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOREV.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.elevator;
+
+public class ElevatorIOREV implements ElevatorIO{
+  /** The gear ratio between the motor and the elevator mechanism */
+  private static final double GEAR_RATIO = 2.0;
+
+
+
+}
