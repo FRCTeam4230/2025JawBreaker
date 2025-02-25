02010d2f76d1f92ffd6b358db5e07ddca304f3ec
Chris Morton <chris.morton@certifi.com>
2025-02-24 21:12:05 -0600

    updated fix

--- src/main/java/frc/robot/commands/ScoringCommands.java
+++ src/main/java/frc/robot/commands/ScoringCommands.java
@@ -47,13 +47,9 @@ public class ScoringCommands {
   }
 
   public Command topLevel() {
-<<<<<<< Updated upstream
     return Commands.sequence(
             Commands.waitUntil(claw::hasCoral),
             Commands.parallel(claw.hold().until(() -> !claw.hasCoral()), arm.L2(), elevator.L4()))
-=======
-    return Commands.sequence(Commands.waitUntil(claw::hasCoral), Commands.parallel(arm.L2(),claw.hold(),elevator.L4()))
->>>>>>> Stashed changes
         .withName("topLevel");
 
     //    return new FunctionalCommand(
--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -6,6 +6,8 @@
 
 package frc.robot.subsystems.arm;
 
+import static edu.wpi.first.units.Units.*;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.Alert;
@@ -15,12 +17,9 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SelectCommand;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.subsystems.DefaultCurrentCommandLoggableSubsystem;
+import java.util.Map;
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
-
-import java.util.Map;
-
-import static edu.wpi.first.units.Units.*;
 
 /**
  * The Arm subsystem controls a dual-motor arm mechanism for game piece manipulation. It supports
